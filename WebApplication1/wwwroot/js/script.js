60905e8e13ab7e02c18f84031ccd0666816640b4
KyberCritter <88512946+KyberCritter@users.noreply.github.com>
2024-09-25 12:35:47 -0500

    more files?

---
+++ WebApplication1/wwwroot/js/script.js
@@ -0,0 +1,79 @@
+
+
+var len;
+var results = '';
+
+function apiSearch() {
+  var params = {
+    "q": $("#query").val(),
+    "count": "50",
+    "offset": "0",
+    "mkt": "en-us"
+  };
+
+  $.ajax({
+      url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+      beforeSend: function (xhrObj) {
+          xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "a3f3cb709d3f48429c104cca76d3edea");
+      },
+      type: "GET",
+    })
+    .done(function (data) {
+        len = data.webPages.value.length;
+        for (i = 0; i < len; i++) {
+        results += "<p><a href='" + data.webPages.value[i].url + "'>" + data.webPages.value[i].name + "</a>: " + data.webPages.value[i].snippet + "</p>";
+        }
+
+        $('#searchResults').html(results);
+        $('#searchResults').dialog();
+    })
+    .fail(function () {
+        alert("error");
+    });
+}
+
+var input = document.getElementById("query");
+
+input.addEventListener("keypress", function (event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        document.getElementById("searchButton").click();
+    }
+});
+
+function luckySearch() {
+    var params = {
+        "q": $("#query").val(),
+        "count": "50",
+        "offset": "0",
+        "mkt": "en-us"
+    };
+
+    $.ajax({
+        url: 'https://api.bing.microsoft.com/v7.0/search?' + $.param(params),
+        beforeSend: function (xhrObj) {
+            xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "a3f3cb709d3f48429c104cca76d3edea");
+        },
+        type: "GET",
+    })
+    .done(function (data) {
+        len = data.webPages.value.length;
+        window.location.href = data.webPages.value[0].url;
+    })
+    .fail(function () {
+        alert("error");
+    });
+}
+
+function changeImage() {
+    document.getElementById("body").classList.toggle('body_2');
+}
+
+function showTime() {
+    const currentTime = new Date();
+    const formattedTime = currentTime.toJSON();
+    $("#time").text(`${currentTime.getHours()}:${currentTime.getMinutes()}`);
+    $(function () {
+        $("#time").dialog();
+    });
+}
